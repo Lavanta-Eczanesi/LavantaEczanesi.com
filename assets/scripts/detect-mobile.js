97d2323d2d89a4d92b8512fc9d84d3b6454881cf
A. Yasin A <ayasinakalin@gmail.com>
2022-06-01 00:30:22 +0300

    Update detect-mobile.js  -- Change threshold, add references.

--- assets/scripts/detect-mobile.js
+++ assets/scripts/detect-mobile.js
@@ -1,1 +1,4 @@
-let mql = window.matchMedia('(max-width: 600px)');
+// References
+// https://developer.mozilla.org/en-US/docs/web/api/window/matchmedia#usage_notes
+// https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList/change_event
+let mql = window.matchMedia('(max-width: 660px)');
