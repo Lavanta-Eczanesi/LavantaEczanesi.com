34b06e05ef99fc54b15cc2384ba1b0380752a09d
A. Yasin A <ayasinakalin@gmail.com>
2024-01-14 00:55:17 +0300

    Update console.log messages to make them clearer to understand

--- assets/scripts/dark-light-change-events.js
+++ assets/scripts/dark-light-change-events.js
@@ -27,7 +27,7 @@ const lightModeMediaQuery = window.matchMedia("(prefers-color-scheme: light)");
 
 darkModeMediaQuery.addEventListener("change", (e) => {
   const isDarkModeOn = e.matches;
-  console.log(`Dark mode is ${isDarkModeOn ? '🌒 switched on' : '☀️ switched off'}.`);
+  console.log(`Dark mode is ${isDarkModeOn ? 'switched on. It is Batman time 🌒 ' : 'switched off. Morning sunshine! ☀️'}.`);
   if (isDarkModeOn){
     wrapper(darkenMap);
     wrapper(darkenUi);
@@ -36,7 +36,7 @@ darkModeMediaQuery.addEventListener("change", (e) => {
 
 lightModeMediaQuery.addEventListener("change", e => {
   const isLightModeOn = e.matches;
-  console.log(`Light mode is ${isLightModeOn ? '☀️ switched on' : '🌒 switched off'}.`);
+  console.log(`Light mode is ${isLightModeOn ? 'switched on. Put your glasses on ☀️' : 'switched off. Getting dark 🌒'}.`);
   if (isLightModeOn){
     wrapper(lightenMap);
     wrapper(lightenUi);
